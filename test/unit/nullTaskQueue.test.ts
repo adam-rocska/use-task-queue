812cca57dc4b4c57dadc388ed5ec8db0c59f26ce
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-12 22:57:42 +0200

    nice

--- test/unit/nullTaskQueue.test.ts
+++ test/unit/nullTaskQueue.test.ts
@@ -1,9 +1,46 @@
-import {nullTaskQueue} from '#nullTaskQueue';
+import {isNullTaskQueue, nullTaskQueue} from '#nullTaskQueue';
 import {isTaskQueueHook} from '#TaskQueueHook';
 
 describe('nullTaskQueue', () => {
-  it('should return a new TaskQueueHook', () => {
-    const queue = nullTaskQueue();
-    expect(isTaskQueueHook(queue)).toBe(true);
+  describe('.nullTaskQueue()', () => {
+    it('should return a new TaskQueueHook', () => {
+      const queue = nullTaskQueue();
+      expect(isTaskQueueHook(queue)).toBe(true);
+    });
+
+    it('should return a TaskQueueHook with all null fields.', () => {
+      const queue = nullTaskQueue<number, number>();
+      queue.push(123);
+      expect(queue.input).toEqual([]);
+      expect(queue.process).toEqual([]);
+      expect(queue.output).toEqual([]);
+      expect(queue.error).toEqual([]);
+      queue.kill({} as any);
+      expect(queue.process).toEqual([]);
+    });
+  });
+
+  describe('.isNullTaskQueue()', () => {
+    it('should return true for a null TaskQueueHook', () => {
+      const queue = nullTaskQueue();
+      expect(isNullTaskQueue(queue)).toBe(true);
+    });
+
+    it('should return false for a non-null TaskQueueHook', () => {
+      expect(
+        isNullTaskQueue({
+          input: [],
+          process: [],
+          output: [],
+          error: [],
+          push: () => {
+            throw new Error('Function not implemented.');
+          },
+          kill: () => {
+            throw new Error('Function not implemented.');
+          },
+        })
+      ).toBe(false);
+    });
   });
 });
--- useTaskQueue/nullTaskQueue.ts
+++ useTaskQueue/nullTaskQueue.ts
@@ -1,25 +1,20 @@
 import {TaskQueueHook} from './TaskQueueHook';
 
 export function nullTaskQueue<I, O>(): TaskQueueHook<I, O> {
-  return {
-    input: nullQueue,
-    process: nullQueue,
-    output: nullQueue,
-    error: nullQueue,
-    push: noop,
-    kill: noop,
-  };
+  return nullTaskQueueHook;
 }
 
 export function isNullTaskQueue<I, O>(queue: TaskQueueHook<I, O>): boolean {
-  if (queue.input !== nullQueue) return false;
-  if (queue.process !== nullQueue) return false;
-  if (queue.output !== nullQueue) return false;
-  if (queue.error !== nullQueue) return false;
-  if (queue.push !== noop) return false;
-  if (queue.kill !== noop) return false;
-  return true;
+  return queue === nullTaskQueueHook;
 }
 
 const noop: (input: any) => void = () => {};
 const nullQueue: readonly any[] = Object.freeze([]);
+const nullTaskQueueHook = {
+  input: nullQueue,
+  process: nullQueue,
+  output: nullQueue,
+  error: nullQueue,
+  push: noop,
+  kill: noop,
+};
