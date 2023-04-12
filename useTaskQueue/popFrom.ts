b19ef60a0f74d23ebcf54888edad13979dcd8480
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-12 22:15:28 +0200

    removed infos. Our logstyle is not mainstream.

--- useTaskQueue/popFrom.ts
+++ useTaskQueue/popFrom.ts
@@ -9,15 +9,6 @@ export default function popFrom<V>(
   const lastItem = queue[lastItemIndex];
   set(queue => {
     const itemIndex = queue.findIndex(v => v === lastItem);
-    if (itemIndex === -1) {
-      console.info('Item already removed.', lastItem);
-    } else if (itemIndex < lastItemIndex) {
-      console.info('Item shifted towards the front.', lastItem);
-    } else if (itemIndex > lastItemIndex) {
-      console.info('Item shifted towards the back.', lastItem);
-    } else {
-      console.info('Item still found in its original position.', lastItem);
-    }
     return queue.filter((_, index) => index !== itemIndex);
   });
   return lastItem;
