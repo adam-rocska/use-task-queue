ec0a34f7bc1850a7f77a737b87258acb0c2b0177
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-13 21:29:18 +0200

    precondition tests added

---
+++ test/uat/uat-4.test.ts
@@ -0,0 +1,155 @@
+import {act, renderHook} from '@testing-library/react-hooks';
+import useTaskQueue from '#useTaskQueue';
+import {descriptors} from '#consistencyGuard';
+import {json} from '@21gram-consulting/ts-codec';
+import TaskError from '#TaskError';
+import {pseudoCancellable} from 'real-cancellable-promise';
+
+beforeEach(() => {
+  descriptors.clear();
+});
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Simple hook precondition failure', () => {
+  test('Produces a simple precondition failure for a synchronous task.', async () => {
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: v => [v * 2],
+        precondition: () => false,
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, undefined)],
+    });
+  });
+
+  test('Produces a reasoned precondition failure for a synchronous task.', async () => {
+    const error = new Error('test');
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: v => [v * 2],
+        precondition: () => {
+          throw error;
+        },
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current.error[0]).toBeInstanceOf(TaskError);
+    const taskError = result.current.error[0] as TaskError<number, number>;
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, error)],
+    });
+
+    expect(taskError);
+  });
+
+  test('Produces a simple precondition failure for a asynchronous task.', async () => {
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: async v => [v * 2],
+        precondition: () => false,
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, undefined)],
+    });
+  });
+
+  test('Produces a reasoned precondition failure for a asynchronous task.', async () => {
+    const error = new Error('test');
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: async v => [v * 2],
+        precondition: () => {
+          throw error;
+        },
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, error)],
+    });
+  });
+
+  test('Produces a simple precondition failure for a cancellable asynchronous task.', async () => {
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: v => pseudoCancellable(Promise.resolve([v * 2])),
+        precondition: () => false,
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, undefined)],
+    });
+  });
+
+  test('Produces a reasoned precondition failure for a cancellable asynchronous task.', async () => {
+    const error = new Error('test');
+    const {result, rerender} = renderHook(() =>
+      useTaskQueue({
+        name: 'test',
+        codec: json.number,
+        task: v => pseudoCancellable(Promise.resolve([v * 2])),
+        precondition: () => {
+          throw error;
+        },
+      })
+    );
+
+    rerender();
+    act(() => result.current.push(2));
+
+    expect(result.current).toBeInState({
+      input: [],
+      process: [],
+      output: [],
+      error: [new TaskError('test', 'Precondition failed.', 2, error)],
+    });
+  });
+});
