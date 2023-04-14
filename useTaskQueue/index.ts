19f72c81c8d46e1ed19f8f0d30b9d6acfce71f11
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-14 18:32:58 +0200

    Exposing API

--- useTaskQueue/index.ts
+++ useTaskQueue/index.ts
@@ -1,11 +1,11 @@
 export {default as useTaskQueue} from './useTaskQueue';
 export {default as InconsistencyError} from './InconsistencyError';
-export {default as TaskError} from './TaskError';
 export {default as Task} from './Task';
 
 export * from './nullTaskQueue';
 export * from './TaskQueueDescriptor';
 
-export {TaskOutput} from './TaskOutput';
-export {TaskProcess} from './TaskProcess';
-export {TaskQueueHook} from './TaskQueueHook';
+export {default as TaskError} from './TaskError';
+export {TaskOutput, isTaskOutput} from './TaskOutput';
+export {TaskProcess, isTaskProcess} from './TaskProcess';
+export {TaskQueueHook, isTaskQueueHook} from './TaskQueueHook';
