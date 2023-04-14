bfe4976686366c1ab1db39e7b4f4fd6d8a80d630
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2023-04-14 08:49:28 +0200

    couldn't do any better now ¯\_(ツ)_/¯

--- test/unit/flushFrom.test.ts
+++ test/unit/flushFrom.test.ts
@@ -1,11 +1,8 @@
 import flushFrom from '#flushFrom';
 
 describe('flushFrom', () => {
-  it("Shouldn't resort to set if the items to remove is empty.", () => {
-    const set = jest.fn();
-    flushFrom(set, []);
-    expect(set).not.toHaveBeenCalled();
-  });
+  let setStateDispatch = jest.fn();
+  beforeEach(() => (setStateDispatch = jest.fn()));
 
   it('Should remove the items from the set.', () => {
     const set = jest.fn();
@@ -20,18 +17,97 @@ describe('flushFrom', () => {
     expect(setCall([1, 3, 4, 5, 2, 1, 2, 3])).toEqual([4, 5, 1, 2, 3]);
   });
 
-  describe('in sync', () => {
-    it('should flush and return all.', () => {});
-    it('should flush and return from head.', () => {});
-    it('should flush and return from tail.', () => {});
-    it('should flush and return from in-between.', () => {});
-    it('should flush and return from scattered.', () => {});
+  it("Shouldn't resort to set if the items to remove is empty.", () => {
+    const result = flushFrom(setStateDispatch, []);
+    expect(setStateDispatch).not.toHaveBeenCalled();
+    expect(result).toEqual([]);
   });
+  it("Shouldn't resort to set if the queue is empty.", () => {
+    const result = flushFrom(setStateDispatch, []);
+    expect(setStateDispatch).not.toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+  it("Shouldn't resort to set if both the queue and the items to remove are empty.", () => {
+    const result = flushFrom(setStateDispatch, [], []);
+    expect(setStateDispatch).not.toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  describe('in sync', () => {
+    it('should flush and return all.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([1, 2, 3]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3])).toEqual([]);
+    });
+    it('should flush and return from head.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3], [1, 2]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([1, 2]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3])).toEqual([3]);
+    });
+    it('should flush and return from tail.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3], [2, 3]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2, 3]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3])).toEqual([1]);
+    });
+    it('should flush and return from in-between.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3], [2]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3])).toEqual([1, 3]);
+    });
+    it('should flush and return from scattered.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3, 4, 5], [2, 4]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2, 4]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3, 4, 5])).toEqual([
+        1, 3, 5,
+      ]);
+    });
+  });
+
   describe('out of sync', () => {
-    it('should flush and return all.', () => {});
-    it('should flush and return from head.', () => {});
-    it('should flush and return from tail.', () => {});
-    it('should flush and return from in-between.', () => {});
-    it('should flush and return from scattered.', () => {});
+    it('should flush and return all.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([1, 2, 3]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3, 4, 5])).toEqual([
+        4, 5,
+      ]);
+    });
+    it('should flush and return from head.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3], [1, 2]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([1, 2]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 1, 2, 3])).toEqual([
+        1, 2, 3,
+      ]);
+    });
+    it('should flush and return from tail.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3], [2, 3]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2, 3]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 3, 1, 2])).toEqual([
+        1, 1, 2,
+      ]);
+    });
+    it('should flush and return from in-between.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3, 4], [2, 3]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2, 3]);
+      expect(setStateDispatch.mock.calls[0][0]([1, 2, 1, 2, 3])).toEqual([
+        1, 1, 2,
+      ]);
+    });
+    it('should flush and return from scattered.', () => {
+      const result = flushFrom(setStateDispatch, [1, 2, 3, 4, 5], [2, 4]);
+      expect(setStateDispatch).toHaveBeenCalled();
+      expect(result).toEqual([2, 4]);
+      expect(
+        setStateDispatch.mock.calls[0][0]([1, 2, 3, 4, 5, 1, 2, 3])
+      ).toEqual([1, 3, 5, 1, 2, 3]);
+    });
   });
 });
