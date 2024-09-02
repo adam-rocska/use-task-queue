9ca500d698f0e7f95c990324ee21b4d5382533be
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-09-02 14:52:28 +0200

    🧹 Removed dumb comment block.

--- src/task-queue-descriptor.ts
+++ src/task-queue-descriptor.ts
@@ -11,17 +11,6 @@ export interface TaskQueueDescriptor<I, O> {
   readonly postcondition?: (input: I, output: O) => boolean;
 }
 
-/**
- * @summary
- * Creates a descriptor for a task queue.
- * @description
- * This is a convenience function that allows you to create a descriptor for a task queue.
- * It's handy for use when manual typing is inconvenient.
- * @param descriptor - The descriptor to create.
- * @returns The typed descriptor, unchanged.
- * @typeParam I - The type of the input to the task queue.
- * @typeParam O - The type of the output of the task queue.
- */
 export const TaskQueueDescriptor = <I, O>(
   descriptor: TaskQueueDescriptor<I, O>
 ) => descriptor;
